05095df49740b3e9d45a10d1c9abd2d243e3f9e1
xushengming <6213857@gmail.com>
2019-11-23 00:08:18 +0800

    modify _41

--- src/leetcode/editor/en/_41_FirstMissingPositive.java
+++ src/leetcode/editor/en/_41_FirstMissingPositive.java
@@ -98,6 +98,25 @@ class Solution {
         }
         return minMiss;
     }
+    public int firstMissingPositive3(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++){
+            while (nums[i] > 0 && nums[i] <= nums.length
+                    && nums[nums[i] - 1] != nums[i]){
+                int tmp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
